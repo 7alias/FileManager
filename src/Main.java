e67b7ddbaf003ed178500c6bfb4fca550ab87a5f
Ilya Maximov <xc.navi@gmail.com>
2021-12-21 14:51:14 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
             System.out.println("Listing contents of: "+ file.getPath());
             for (int i =0; i< files.length; i++){
                 System.out.println(files[i]);
+                
             }
         }
 
