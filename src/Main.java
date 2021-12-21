88f1621bf6bb62ae243c82a8782a1b6a320f5e4c
Ilya Maximov <xc.navi@gmail.com>
2021-12-21 14:16:33 +0200

    file manager

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Main {
+
+
+        public static void main(String[] args) {
+            File file = new File("C:\\Users\\Ilmax\\Documents\\IDEA\\2021_practice_week");
+            String [] files = file.list();
+
+            System.out.println("Listing contents of: "+ file.getPath());
+            for (int i =0; i< files.length; i++){
+                System.out.println(files[i]);
+            }
+        }
+
+
+}
