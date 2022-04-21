c65d638a5a09fcc9cad9443bc95cba320ce371a9
Ilya Maximov <xc.navi@gmail.com>
2022-04-21 10:21:36 +0200

    ok

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,25 @@ import java.io.File;
 public class Main {
 
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+
+
+        int num = 0;
+        for (int i = 0; i < 100; i++){
+           num= num++;
+            System.out.println(num);
+        }
+            /*
             File file = new File("C:\\Users\\Ilmax\\Documents\\IDEA\\2021_practice_week");
             String [] files = file.list();
 
             System.out.println("Listing contents of: "+ file.getPath());
             for (int i =0; i< files.length; i++){
                 System.out.println(files[i]);
-                
-            }
-        }
-
-
+*/
+    }
 }
+
+
+
+
